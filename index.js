9a7c9bc55facbc966754efec724af2febcd5712b
honorable-scraper-2425 <kdraper93@gmail.com>
2019-06-25 18:17:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // write your code below!
-function happyHolidays (name) {
+function happyHolidays() {
+  return `Happy holidays!`
+}
+
+function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 function happyHolidayTo (holiday, name) {
